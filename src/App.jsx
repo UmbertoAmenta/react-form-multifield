48093fe0b4ab61ad0ff7f8e7cd7d5de2360c3318
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-04 19:30:38 +0100

    feat: data (from string to object), form (now multi-input)

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,116 @@
 import { useState } from "react";
 
 export default function App() {
-  const [postTitle, setPostTitle] = useState("");
+  const [formData, setFormData] = useState({
+    id: 1,
+    title: "",
+    content: "",
+    author: "",
+    sector: "",
+  });
   const [list, setList] = useState([]);
 
-  const addTitle = (event) => {
+  const handleSubmit = (event, id) => {
     event.preventDefault();
-    const currentList = [...list, postTitle];
+    const currentList = [...list, formData];
     setList(currentList);
 
     // alternative
-    // setList([...list, postTitle])
-    // setList((current) => [...current, postTitle]);
+    // setList([...list, formData])
+    // setList((current) => [...current, formData]);
 
-    setPostTitle("");
+    setFormData({
+      id: 1,
+      title: "",
+      content: "",
+      author: "",
+      sector: "",
+    });
   };
 
-  const deleteList = (index) => setList([]);
+  const deleteList = () => setList([]);
 
-  const deleteTitle = (index) => {
+  const deletePost = (id) => {
     setList((current) => {
-      return current.filter((postTitle, postIndex) => postIndex !== index);
+      return current.filter((formData, postid) => postid !== id);
     });
+  };
+
+  const handlerFormData = (field, value) => {
+    setFormData((formData) => ({
+      ...formData,
+      [field]: value,
+    }));
   };
 
   return (
     <div className="container">
       <h2>Blog in costruzione</h2>
-      <ul>
-        {list.map((title, index) => {
-          return (
-            <li key={index}>
-              {title}
-              <button onClick={() => deleteTitle(index)}>🗑️</button>
-            </li>
-          );
-        })}
-      </ul>
-      <form onSubmit={addTitle}>
-        <input
-          type="text"
-          placeholder="Digita Titolo da aggiungere"
-          value={postTitle}
-          onChange={(event) => {
-            setPostTitle(event.target.value);
-          }}
-        />
-        <button type="submit">
-          <strong>+</strong> Nuovo titolo
-        </button>
+      <div>
+        <ul>
+          {list.map((post, id) => {
+            return (
+              <li key={id}>
+                {post.title} - {post.author} - {post.sector}
+                <button onClick={() => deletePost(id)}>🗑️</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit}>
         <button type="button" onClick={deleteList}>
           Svuota lista
+        </button>
+
+        <label htmlFor="postTitle">Titolo</label>
+        <input
+          id="postTitle"
+          type="text"
+          placeholder="..."
+          value={formData.title}
+          onChange={(event) => {
+            handlerFormData("title", event.target.value);
+          }}
+        />
+
+        <label htmlFor="postContent">Contenuto</label>
+        <input
+          id="postContent"
+          type="text"
+          placeholder="..."
+          value={formData.content}
+          onChange={(event) => {
+            handlerFormData("content", event.target.value);
+          }}
+        />
+
+        <label htmlFor="postAuthor">Autore</label>
+        <input
+          id="postAuthor"
+          type="text"
+          placeholder="..."
+          value={formData.author}
+          onChange={(event) => {
+            handlerFormData("author", event.target.value);
+          }}
+        />
+
+        <label htmlFor="postSector">Settore</label>
+        <select
+          name="postSector"
+          id="postSector"
+          onChange={(event) => {
+            handlerFormData("sector", event.target.value);
+          }}
+        >
+          <option value="frontEnd">FrontEnd</option>
+          <option value="backEnd">BackEnd</option>
+          <option value="design">UI/UX</option>
+        </select>
+
+        <button type="submit">
+          <strong>+</strong> Nuovo post
         </button>
       </form>
     </div>
